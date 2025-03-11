e55e3c5d41367e83b9851f9c9bdf566e091bfbfc
Maria Treier <137496420+belosnezhie@users.noreply.github.com>
2025-03-11 17:31:40 +0100

    Task 5 (Integration with S3) (#4)
    * feat: update import path
    
    * feat: update URL path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://hadypg3ce2.execute-api.eu-central-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://449178a8f5.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
