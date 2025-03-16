2f7cbc1f597cbe4d2a9f71b4624125405fbff4f2
Maria Treier <mariatraier@gmail.com>
2025-03-17 00:58:05 +0400

    feat: update URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://3tu2pw8ur5.execute-api.eu-central-1.amazonaws.com/prod/",
+  product: "https://ec90re3gi1.execute-api.eu-central-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://uk1zdy3pv4.execute-api.eu-central-1.amazonaws.com/prod",
+  import: "https://m75dxy9k41.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
