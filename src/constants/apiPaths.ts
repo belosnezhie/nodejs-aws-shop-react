1315e2fd3105d28e4a9c25192d8e023d28a72973
Maria Treier <mariatraier@gmail.com>
2025-03-16 23:24:08 +0400

    feat: update URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://rc25bhgq9l.execute-api.eu-central-1.amazonaws.com/prod/",
+  product: "https://3tu2pw8ur5.execute-api.eu-central-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://v3gkeerfbe.execute-api.eu-central-1.amazonaws.com/prod",
+  import: "https://uk1zdy3pv4.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
