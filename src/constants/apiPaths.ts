aa56d96794ca86ad17c352721ec27c420a825910
Maria Treier <mariatraier@gmail.com>
2025-04-14 01:07:10 +0200

    feat: update URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://hh4tr3tvhf.execute-api.eu-central-1.amazonaws.com/prod/",
   order:
-    "https://fgjfwopw66w42sljjraa4pji2y0nxtuz.lambda-url.eu-central-1.on.aws/api/profile/cart",
+    "http://belosnezhie-cart-api-production-large.eu-central-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://ev9z99ja89.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://fgjfwopw66w42sljjraa4pji2y0nxtuz.lambda-url.eu-central-1.on.aws/api",
+  cart: "http://belosnezhie-cart-api-production-large.eu-central-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
