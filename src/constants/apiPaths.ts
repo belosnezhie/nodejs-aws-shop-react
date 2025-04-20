dfbdbfb524dd319ae0a45fe18ba28faa76a9896f
Maria Treier <mariatraier@gmail.com>
2025-04-20 23:21:57 +0200

    feat: update path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order:
     "http://belosnezhie-cart-api-production-small.eu-central-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://ev9z99ja89.execute-api.eu-central-1.amazonaws.com/prod",
-  bff: "http://belosnezhie-bff-api-production.eu-central-1.elasticbeanstalk.com",
+  bff: "http://belosnezhie-bff-api-production-bff-small.eu-central-1.elasticbeanstalk.com",
   cart: "http://belosnezhie-cart-api-production-small.eu-central-1.elasticbeanstalk.com/api",
 };
 
